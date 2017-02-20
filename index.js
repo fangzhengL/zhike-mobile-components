929e7096064dceb040929073a5b1ed66b97fcb81
wansong <betterofsong@gmail.com>
2017-02-21 01:26:12 +0800

    1.0.3

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 import EmptyView from './EmptyView';
 import LoadingView from './LoadingView';
 import SimpleCalendar './SimpleCalendar';
-import AnimatedDigit from './AnimatedDigit';
 import SeparatorLine './ZKSeparatorLine';
 import ProgressBar from './ProgressBar';
 import TextInput from  './ZKTextInput';
@@ -12,7 +11,6 @@ const ex = {
   EmptyView,
   LoadingView,
   SimpleCalendar,
-  AnimatedDigit,
   SeparatorLine,
   ProgressBar,
   TextInput,
