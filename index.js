6542bd6375210d8954feb41907691b6e68765aa7
wansong <betterofsong@gmail.com>
2017-06-13 14:54:49 +0800

    1.1.1: trivia

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ import LoadingView_ from './LoadingView';
 import SimpleCalendar_ from './SimpleCalendar';
 import SeparatorLine_ from './ZKSeparatorLine';
 import TextInput_ from  './ZKTextInput';
-import AnimatedAlertContainer from './AnimatedAlertContainer';
+import AnimatedAlertContainer_ from './AnimatedAlertContainer';
 
 export const EmptyView = EmptyView_;
 export const LoadingView = LoadingView_;
 export const SimpleCalendar = SimpleCalendar_;
 export const SeparatorLine = SeparatorLine_;
 export const TextInput = TextInput_;
+export const AnimatedAlertContainer = AnimatedAlertContainer_;
 
 export default {
   EmptyView,
